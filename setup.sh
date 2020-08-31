b89390caf7d3ba5d35f167eb480499c5568b91dc
JamieHoSzeYui <wileylau@gmail.com>
2020-08-31 08:31:01 +0800

    Add some stuff

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+#git clone --quiet --recurse-submodules https://github.com/erfanoabdi/ErfanGSIs ErfanGSIs >> /dev/null
+
+if [[ $( ls /usr/bin | grep telegram) ]]; then
+    echo "Telegram is Installed. Skip."
+else
+    sudo install telegram /usr/bin
+fi
+
+TOKEN=$(cat token.txt)
+
+if [[ $( ls /etc | grep telegram.sh.conf) ]]; then
+   echo "Bot Token configured. Skip."
+else
+    sudo touch /etc/telegram.sh.conf
+    sudo echo "TOKEN=$TOKEN" >> "/etc/telegram.sh.conf"
+fi
+
+telegram -M "bruh"
+
+
