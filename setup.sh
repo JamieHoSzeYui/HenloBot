8c326980bd0de12ba2dec805e8012708c3675766
henloboi <henloboii@protonmail.ch>
2020-08-31 08:34:01 +0800

    install url2gsi

--- setup.sh
+++ setup.sh
@@ -2,12 +2,18 @@
 
 
 
-#git clone --quiet --recurse-submodules https://github.com/erfanoabdi/ErfanGSIs ErfanGSIs >> /dev/null
+git clone --quiet --recurse-submodules https://github.com/erfanoabdi/ErfanGSIs ErfanGSIs >> /dev/null
 
 if [[ $( ls /usr/bin | grep telegram) ]]; then
     echo "Telegram is Installed. Skip."
 else
     sudo install telegram /usr/bin
+fi
+
+if [[ $( ls /usr/bin | grep url2gsi) ]]; then
+     echo "url2GSI binary exists. Skip."
+else
+    sudo install url2gsi /usr/bin
 fi
 
 TOKEN=$(cat token.txt)
